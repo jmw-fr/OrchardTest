3e5defc00f8a60fb2e91ba88868232b245247720
Jean-Marc Weeger <jmweeger@ugc.fr>
2019-12-24 10:43:47 +0100

    [OrchardTest] Projet ASP.Net Core 3.0

--- OrchardTest/Startup.cs
+++ OrchardTest/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace OrchardTest
 {
@@ -18,9 +19,9 @@ namespace OrchardTest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
             }
